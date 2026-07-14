367b3881fd45f037302779f841773b20785a62cf
shweta <shwetacp29@gmail.com>
2026-07-14 22:24:06 +0530

    Added Variables program

---
+++ Basics/Variables.java
@@ -0,0 +1,15 @@
+public class Variables{
+public static void main(String[] args){
+int age=20;
+double height=171.5;
+char grade="A";
+boolean isStudent=true;
+String name="shweta";
+
+System.out.println("Name":+name);
+System.out.println("Age":+age);
+System.out.println("Height":+height);
+System.out.println("Grade":+grade);
+System.out.println("Student":+isStudent);
+}
+}
