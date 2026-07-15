46e6a574f7a9368d5ba6e403da0e873a652c9d8b
shweta <shwetacp29@gmail.com>
2026-07-15 21:35:34 +0530

    Added Operators program

---
+++ Basics/Operators.java
@@ -0,0 +1,12 @@
+public class Operators {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 5;
+
+        System.out.println("Addition = " + (a + b));
+        System.out.println("Subtraction = " + (a - b));
+        System.out.println("Multiplication = " + (a * b));
+        System.out.println("Division = " + (a / b));
+        System.out.println("Remainder = " + (a % b));
+    }
+}
